d6d4afdab5b9c670bf509b47ef1688b85ed38324
Anwar Ali <anwar08ali@gmail.com>
2022-09-22 17:08:35 +0530

    home page

--- src/pages/home/rowTemplate.js
+++
@@ -1,5 +0,0 @@
-export const rowTemplate = (item) => (
-  <div className="item" key={item.index}>
-    {item.text}
-  </div>
-);
--- src/pages/home/settings.js
+++
@@ -1,9 +0,0 @@
-export const SETTINGS = {
-  minIndex: 1,
-  maxIndex: 16,
-  startIndex: 6,
-  numOfRows: 5,
-  rowsHeight: 20,
-  //   virtualRows: 2,
-  //   tolerance: 2, // VirtualRows and tolerance are same just use any one. If tolerance need to make changes in virtual scroller comp
-};
