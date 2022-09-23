d590639abdb0407debaec937b41b8ac51cbdc56f
Anwar Ali <anwar08ali@gmail.com>
2022-09-23 13:01:41 +0530

    service worker push notification

--- custom-sw-cache-fullApp.js
+++ custom-sw-cache-fullApp.js
@@ -25,62 +25,71 @@ self.addEventListener("activate", (e) => {
 // call fetch event
 self.addEventListener("fetch", (e) => {
   console.log("SW: Fetching...");
-  console.warn("url:", navigator.online,navigator);
-  if (!navigator.online) {
-    if (
-      e.request.url === "http://localhost:5500/index.js" ||
-      e.request.url === "https://anwar08ali.github.io/index.js"
-    ){
-      e.waitUntil(
-        this.registration.showNotification("Internet is not working", {
-          body: "App is running in offline mode.",
-        })
-      );
-    }
-      
-  }
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-
-      // IMPORTANT: Clone the request. A request is a stream and
-      // can only be consumed once. Since we are consuming this
-      // once by cache and once by the browser for fetch, we need
-      // to clone the response
-      var fetchRequest = e.request.clone();
-
-      return fetch(fetchRequest).then(function (response) {
-        // Check if we received a valid response
-        if (!response || response.status !== 200 || response.type !== "basic") {
+  console.warn("url:", e.request.url);
+  var xhr = XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHttp');
+  xhr.onload = function(){
+    console.warn("Online");
+    e.respondWith(
+      caches.match(e.request).then(function (response) {
+        // Cache hit - return response
+        if (response) {
           return response;
         }
-
-        // IMPORTANT: Clone the response. A response is a stream
-        // and because we want the browser to consume the response
-        // as well as the cache consuming the response, we need
-        // to clone it so we have 2 stream.
-        var responseToCache = response.clone();
-
-        caches.open(cacheName).then(function (cache) {
-          cache.put(e.request, responseToCache);
+  
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response
+        var fetchRequest = e.request.clone();
+  
+        return fetch(fetchRequest).then(function (response) {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== "basic") {
+            return response;
+          }
+  
+          // IMPORTANT: Clone the response. A response is a stream
+          // and because we want the browser to consume the response
+          // as well as the cache consuming the response, we need
+          // to clone it so we have 2 stream.
+          var responseToCache = response.clone();
+  
+          caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, responseToCache);
+          });
+  
+          return response;
         });
-
-        return response;
-      });
-    })
-    // fetch(e.request)
-    //   .then((res) => {
-    //     //Make copy/clone of resp obj
-    //     const resClone = res.clone();
-    //     //Open cache
-    //     caches.open(cacheName).then((cache) => {
-    //       cache.put(e.request, resClone);
-    //       return res;
-    //     });
-    //   })
-    //   .catch((err) => caches.match(e.request).then((res) => res))
-  );
+      })
+      // fetch(e.request)
+      //   .then((res) => {
+      //     //Make copy/clone of resp obj
+      //     const resClone = res.clone();
+      //     //Open cache
+      //     caches.open(cacheName).then((cache) => {
+      //       cache.put(e.request, resClone);
+      //       return res;
+      //     });
+      //   })
+      //   .catch((err) => caches.match(e.request).then((res) => res))
+    );
+}
+xhr.onerror = function(){
+  console.warn("Offline");
+  if (
+    e.request.url === "http://localhost:5500/index.js" ||
+    e.request.url === "https://anwar08ali.github.io/index.js"
+  ){
+    e.waitUntil(
+      this.registration.showNotification("Internet is not working", {
+        body: "App is running in offline mode.",
+      })
+    );
+  }
+}
+  // if (!navigator.online) {
+    
+      
+  // }
+  
 });
